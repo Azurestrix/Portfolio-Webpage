cf3150bb5320fb19ba191d94b8993d5c0d7e83cc
Domokos-Laszlo-Oliver <108230324+Domokos-Laszlo-Oliver@users.noreply.github.com>
2023-02-26 15:27:32 +0100

    +
    +

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-let $navbar = document.querySelector('.Navbar_Div')
+/*Felugró Ablak */
+let $Projects = document.querySelector('.Navbar_Div_1');
+let $About = document.querySelector('.Navbar_Div_2');
+
+let $Projects_Popup = document.querySelector('#PROJECTS');
+let $About_Popup = document.querySelector('#ABOUT');
 
 
-$navbar.addEventListener('onclick', function(){
-    $navbar.setAttribute( 'color','color: red;')
-})
+
+$Projects.addEventListener('click', visibility_1);
+$About.addEventListener('click', visibility_2);
+
+function visibility_1(){
+    $Projects_Popup.classList.toggle('Visible');
+    $About_Popup.classList.remove('Visible');
+}
+
+function visibility_2(){
+    $About_Popup.classList.toggle('Visible');
+    $Projects_Popup.classList.remove('Visible');
+}
+
+
+/*Felugró Ablak */
+
+let $Loader = document.querySelector('#LOADER')
+
+window.addEventListener('load', invisibility);
+
+function invisibility(){
+    $Loader.classList.add('.Invisible');
+}
+
