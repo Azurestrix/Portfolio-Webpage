df678a2ffb861cd7cc2a54eea5ccdfbdc96fc5bf
Domokos-Laszlo-Oliver <108230324+Domokos-Laszlo-Oliver@users.noreply.github.com>
2023-03-28 15:07:25 +0200

    +
    +

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+/* Loader */
+
+const $Loader = document.querySelector('#LOADER')
+
+/* window.addEventListener('load', invisibility); */   /* Jó lenne konkrétan tudni ez miért is nem megy? */
+
+/* function invisibility(){
+    $Loader.classList.add('.Js_Invisible');
+}
+
+setTimeout(invisibility, 3000) */
+
+
+
+  
+window.addEventListener("load", function () {
+    this.setTimeout(function loader(event) {
+      document
+        .getElementById("preloader-container")
+        .classList.toggle("invisible");
+    }, 7000);
+  });
+
+
+
+
+
+
+/* Loader */
+
+
+
+
 /*Felugró Ablak */
 const $Projects = document.querySelector('.Navbar_Div_1');
 const $About = document.querySelector('.Navbar_Div_2');
@@ -11,30 +44,18 @@ $Projects.addEventListener('click', visibility_1);
 $About.addEventListener('click', visibility_2);
 
 function visibility_1(){
-    $Projects_Popup.classList.toggle('Visible');
-    $About_Popup.classList.remove('Visible');
+    $Projects_Popup.classList.toggle('Js_Visible');
+    $About_Popup.classList.remove('Js_Visible');
 }
 
 function visibility_2(){
-    $About_Popup.classList.toggle('Visible');
-    $Projects_Popup.classList.remove('Visible');
+    $About_Popup.classList.toggle('Js_Visible');
+    $Projects_Popup.classList.remove('Js_Visible');
 }
 
 
 /*Felugró Ablak */
 
-
-/* Loader */
-
-const $Loader = document.querySelector('#LOADER')
-
-window.addEventListener('load', invisibility);
-
-function invisibility(){
-    $Loader.classList.add('.Invisible');
-}
-
-/* Loader */
 
 
 
