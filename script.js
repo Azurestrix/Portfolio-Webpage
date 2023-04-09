55227e41345b5380114f99b7ef36b7f3fb6772c4
Domokos-Laszlo-Oliver <108230324+Domokos-Laszlo-Oliver@users.noreply.github.com>
2023-04-09 14:04:00 +0200

    +
    +

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ setTimeout(invisibility, 3000) */
 
 
   
-window.addEventListener("load", function () {
+/* window.addEventListener("load", function () {
     this.setTimeout(function loader(event) {
       document
         .getElementById("preloader-container")
         .classList.toggle("invisible");
     }, 7000);
   });
-
+ */
 
 
 
@@ -57,5 +57,70 @@ function visibility_2(){
 /*Felugró Ablak */
 
 
+
+/* Carousel */
+const $leftButton = document.querySelector('#LEFT_BUTTON');
+const $rightButton = document.querySelector('#RIGHT_BUTTON');
+const $carouselElements = document.querySelector('#PROJECT_LINK');
+
+let carouselListCurrentPosition = 0;
+
+let currentCarouselList = []
+
+
+const htmlCarouselList = [
+  '<a href="http://"><img src="Images/lili.jpg" alt="1"></a>',
+  '<a href="http://"><img src="Images/Vector_2677.jpg" alt="2"></a>',
+  '<a href="http://"><img src="Images/Vector_2677.jpg" alt="3"></a>',
+  '<a href="http://"><img src="Images/Vector_2677.jpg" alt="4"></a>',
+  '<a href="http://"><img src="Images/dd.jpg" alt="5"></a>'
+]
+
+const pythonCarouselList =[
+  '<a href="http://"><img src="Images/python.jpg" alt="1"></a>',
+  '<a href="http://"><img src="Images/Vector_2677.jpg" alt="2"></a>',
+  '<a href="http://"><img src="Images/Vector_2677.jpg" alt="3"></a>',
+  '<a href="http://"><img src="Images/Vector_2677.jpg" alt="4"></a>',
+  '<a href="http://"><img src="Images/pyther.jpg" alt="5"></a>'
+] 
+
+const $html_Css_Js = document.querySelectorAll('#HTML_CSS_JS_SELECTOR');
+const $python = document.querySelector('');
+
+
+
+$html_Css_Js.addEventListener('click', html_webpage_js);
+
+function html_webpage_js() {
+  currentCarouselList = htmlCarouselList
+  $carouselElements = currentCarouselList
+  console.error('MEGY')
+}
+
+
+
+/* $leftButton.addEventListener('click', leftClick);
+
+function leftClick() {
+  if (carouselListCurrentPosition === 0) {
+    return;
+  } else {
+    $carouselElements.innerHTML = currentCarouselList[carouselListCurrentPosition - 1];
+    carouselListCurrentPosition = carouselListCurrentPosition - 1;
+  }
+}
+
+$rightButton.addEventListener('click', rightClick);
+
+function rightClick() {
+  if (carouselListCurrentPosition === 4) {
+    return;
+  } else {
+    $carouselElements.innerHTML = currentCarouselList[carouselListCurrentPosition + 1];
+    carouselListCurrentPosition = carouselListCurrentPosition + 1;
+  }
+}
+ */
+/* Carousel */
 
 
