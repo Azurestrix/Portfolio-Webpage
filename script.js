1115519037c803e68efa627b198f63534d93166d
Azurestrix <108230324+Azurestrix@users.noreply.github.com>
2023-09-14 14:12:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -49,15 +49,15 @@ let currentCarouselList = [];
 let carouselActivation = false;
 
 const htmlCarouselList = [
-  '<a href="https://github.com/Domokos-Laszlo-Oliver/Final-Portfolio"><figure><img src="Images/Portfolio_Site.jpg" alt="1"><figcaption>This is my portfolio webpage you are on.</figcaption></figure></a>',
-  '<a href="https://domokos-laszlo-oliver.github.io/Mandy/"><figure><img src="Images/Portfolio_webpage.png" alt="2"><figcaption>In Progress</figcaption></figure></a>',
+  '<a href="https://github.com/Azurestrix/Final-Portfolio"><figure><img src="Images/Portfolio_Site.jpg" alt="1"><figcaption>This is my portfolio webpage you are on.</figcaption></figure></a>',
+  '<a href="https://Azurestrix.github.io/Mandy/"><figure><img src="Images/Portfolio_webpage.png" alt="2"><figcaption>In Progress</figcaption></figure></a>',
   '<a href="http://"><figure><img src="Images/Inkedhtmlcssjs_3.jpg" alt="3"><figcaption>HCJ3</figcaption></figure></a>',
   '<a href="http://"><figure><img src="Images/Inkedhtmlcssjs_4.jpg" alt="4"><figcaption>HCJ4</figcaption></figure></a>',
   '<a href="http://"><figure><img src="Images/Inkedhtmlcssjs_5.jpg" alt="5"><figcaption>HCJ5</figcaption></figure></a>',
 ];
 
 const pythonCarouselList = [
-  '<a href="https://github.com/Domokos-Laszlo-Oliver/S.E.R.B."><figure><img src="Images/S.E.R.B..png" alt="S.E.R.B."><figcaption>P1</figcaption></figure></a>',
+  '<a href="https://github.com/Azurestrix/S.E.R.B."><figure><img src="Images/S.E.R.B..png" alt="S.E.R.B."><figcaption>P1</figcaption></figure></a>',
   '<a href="http://"><figure><img src="Images/InkedPython_2.jpg" alt="P2"><figcaption>P2</figcaption></figure></a>',
   '<a href="http://"><figure><img src="Images/InkedPython_3.jpg" alt="P3"><figcaption>P3</figcaption></figure></a>',
   '<a href="http://"><figure><img src="Images/InkedPython_4.jpg" alt="P4"><figcaption>P4</figcaption></figure></a>',
